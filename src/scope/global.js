7be22f6fbdc34807cecf2f742cf7e8bcbe72e4fb
kevindoblea1 <kevin.alvarenga93@gmail.com>
2023-02-26 10:37:57 -0600

    primera clase

---
+++ src/scope/global.js
@@ -0,0 +1,28 @@
+var a; //declaramos variable
+var b = b; //Declaramos y asignamos "inicializamos"
+b = 'bb'; //reasignacion
+var a = 'aa'; //redeclaracion
+
+
+//Global scope
+//cualquier variable que se encuentre en el documento pasa a ser variable global
+//podemos acceder a ella en cualquier parte del documento.
+
+var fruit = 'Apple'; //globalscope
+
+function bestfruit () {
+console.log(fruit);
+}
+
+bestfruit();
+
+//hay que tener cierto cuidado a la hora de declarar una variable
+
+function countries() {
+    country = 'Honduras'; //asignamos el valor como global, sera accedido desde fuera
+    console.log(country); 
+
+}
+
+countries();
+console.log(country);
